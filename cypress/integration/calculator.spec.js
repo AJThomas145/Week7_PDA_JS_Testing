585804cf1c1638b4d0936af5cca4239e30caa3b8
Andrew Thomas <ajthomas145@gmail.com>
2021-11-21 20:27:51 +0000

    tests all passed

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -7,4 +7,63 @@ describe("Calculator", () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+
+  it("do the numbers update the running total", () => {
+    cy.get("#number1").click();
+    cy.get("#operator_add").click();
+    cy.get("#number6").click();
+    cy.get("#operator-equals").click();
+    cy.get("#running-total").should("contain", "7")
+  })
+
+  it("do arithemetical operations update the display with result", () => {
+    cy.get("#number2").click();
+    cy.get("#operator-multiply").click();
+    cy.get("#number2").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should("contain", "4")
+  })
+
+  it("can multiple operations be chained together", () => {
+    cy.get("#number3").click();
+    cy.get("#operator_add").click();
+    cy.get("#number5").click();
+    cy.get("#operator-multiply").click();
+    cy.get("#number3").click();
+    cy.get("#operator-subtract").click();
+    cy.get("#number4").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should("contain", "20")
+  })
+
+  it("is the output as expected for a range of different numbers", () => {
+    cy.get("#number3").click();
+    cy.get("#operator-subtract").click();
+    cy.get("#number5").click();
+    cy.get("#operator_add").click();
+    cy.get("#number1").click();
+    cy.get("#number2").click();
+    cy.get("#number0").click();
+    cy.get("#number3").click();
+    cy.get("#number7").click();
+    cy.get("#number8").click();
+    cy.get("#number5").click();
+    cy.get("#number7").click();
+    cy.get("#number2").click();
+    cy.get("#operator-multiply").click();
+    cy.get("#number0").click();
+    cy.get("#decimal").click();
+    cy.get("#number5").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should("contain", "60189285")
+  })
+
+  it("can numbers be divded by zero", () => {
+    cy.get("#number2").click();
+    cy.get("#operator-divide").click();
+    cy.get("#number0").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should("contain", "undefined")
+  })
+
 })
--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -54,5 +54,45 @@ describe('Calculator', () => {
     const runningTotal = container.find('#running-total');
     expect(runningTotal.text()).toEqual('3');
   })
+
+  it('should be able to concatenate multiple number clicks', () => {
+    container.find('#number2').simulate('click');
+    container.find('#number1').simulate('click');
+    container.find('#number7').simulate('click');
+    container.find('#operator-equals').simulate('click');
+    const runningTotal = container.find('#running-total');
+    expect(runningTotal.text()).toEqual('217');
+  })
+
+  it('should be able to chain multiple operations together', () => {
+    container.find('#number2').simulate('click');
+    container.find('#number1').simulate('click');
+    container.find('#operator-divide').simulate('click');
+    container.find('#number7').simulate('click');    
+    container.find('#operator-subtract').simulate('click');
+    container.find('#number1').simulate('click');
+    container.find('#operator_add').simulate('click');
+    container.find('#number6').simulate('click');
+    container.find('#operator-multiply').simulate('click');
+    container.find('#number2').simulate('click');
+    container.find('#operator-equals').simulate('click');
+    const runningTotal = container.find('#running-total');
+    expect(runningTotal.text()).toEqual('16');
+  })
+
+  it('should be able to clear running total without affecting calculation', () => {
+    container.find('#number2').simulate('click');
+    container.find('#operator_add').simulate('click');
+    container.find('#number7').simulate('click');
+    container.find('#clear').simulate('click');
+    container.find('#number6').simulate('click');
+    container.find('#operator-multiply').simulate('click');
+    container.find('#number2').simulate('click');
+    container.find('#operator-equals').simulate('click');
+    const runningTotal = container.find('#running-total');
+    expect(runningTotal.text()).toEqual('12');
+  })
+
+
 })
 
